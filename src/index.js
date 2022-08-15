9ff2067a53fe3a7434de6a62b62810cfc1e395e8
Patty O'Callaghan <patty19@gmail.com>
2022-08-15 14:25:51 +0100

    Adding missing function and additional comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js';
-import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-database.js';
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-database.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,25 +18,47 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-function writeUserData(studentId, name, imageUrl) {
-  const db = getDatabase(app);
-db.ref('/students').push({
+// ********************* STEP ONE: Process the html form 
 
-      name: name,
-      profile_picture: imageUrl
-  });
-}
-
-// writeUserData('1','Lewis','-')
-
-console.log('hello')
-
+// Get reference to the submit button which id = "send"
 let submitButton = document.getElementById("send");
 
+// Add an event listener, which will work when the submit button is clicked
 submitButton.addEventListener("click", (e) => {
+  
   //Prevent Default Form Submission Behavior
   e.preventDefault();
-let firstName = document.getElementById("fname").value;
-console.log(firstName)
-writeUserData('',firstName,'-')
+
+  // Get the value of the name field in the form
+  let fname = document.getElementById("fname").value;
+  
+  // Print the name in the console, for testing purposes
+  console.log(fname);
+
+  // Call to the function writeStudentData, passing two parameters: name and the value for the profile picture (which is not being used at th emoment)
+  writeStudentData(fname,'image_url');
+
 })
+
+
+//********************* STEP TWO: Create the function that will write the data on the firebase realtime database ***
+
+
+// Write the student data that comes from the html form
+// We are using two parameters: name and the profile picture (which is not being used at the moment)
+function writeStudentData(fname, imageUrl) {
+
+  // Get the database from our firebase app
+  const db = getDatabase(app);
+
+  // Get a reference to the student list on our firebase realtime database
+  const reference = ref(db, '/students');
+
+  // Push the data from the html form to the reference to the student list (see step below)
+  push(reference, {
+      name: fname,
+      profile_picture: imageUrl
+  });
+
+}
+
