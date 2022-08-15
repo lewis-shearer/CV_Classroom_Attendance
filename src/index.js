2eff75eef80982c801b72f9c25d44a23f71ab102
Lewis Shearer <lshearer2957@gmail.com>
2022-08-15 10:07:37 +0100

    First Commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js';
+import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-database.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtlYMju2Du8NDX2h0JVjKrLTg3FzRPv08",
+  authDomain: "beta-project-d59ae.firebaseapp.com",
+  databaseURL: "https://beta-project-d59ae-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "beta-project-d59ae",
+  storageBucket: "beta-project-d59ae.appspot.com",
+  messagingSenderId: "718213421744",
+  appId: "1:718213421744:web:42ba0a3951cf4ca2e7f975"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function writeUserData(studentId, name, imageUrl) {
+  const db = getDatabase(app);
+  const reference = ref(db, 'students/' + studentId)
+
+  set(reference, {
+      name: name,
+      profile_picture: imageUrl
+  });
+}
+
+writeUserData('1','Lewis','-')
+
+console.log('hello')
