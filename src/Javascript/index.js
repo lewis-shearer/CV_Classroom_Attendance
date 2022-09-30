66be665bc8c281ecbcb0ca991c4c6cd90e762a47
Lewis Shearer <lshearer2957@gmail.com>
2022-09-30 20:16:31 +0100

    Updating code
    added page for classes and added code to retrieve data from the database

--- src/Javascript/index.js
+++ src/Javascript/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js';
-import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-database.js';
+import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.6.4/firebase-database.js';
+// Initialize the FirebaseUI Widget using Firebase.
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,12 +19,16 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Get the database from our firebase app
+const db = getDatabase(app);
+
 // ********************* STEP ONE: Process the html form 
 
 // Get reference to the submit button which id = "send"
-let submitButton = document.getElementById("send");
+let submitButton = document.getElementById("sendPupil");
 
 // Add an event listener, which will work when the submit button is clicked
+if (submitButton != null) {
 submitButton.addEventListener("click", (e) => {
   
   //Prevent Default Form Submission Behavior
@@ -43,7 +48,7 @@ submitButton.addEventListener("click", (e) => {
   // Call to the function writeStudentData, passing two parameters: first and last name
   writeStudentData(fname, lname, sID, age, gender, sEmail);
 
-})
+})}
 
 
 //********************* STEP TWO: Create the function that will write the data on the firebase realtime database ***
@@ -53,8 +58,7 @@ submitButton.addEventListener("click", (e) => {
 // We are using two parameters: first name and last name
 function writeStudentData(fname, lname, sID, age, gender, sEmail) {
 
-  // Get the database from our firebase app
-  const db = getDatabase(app);
+
 
   // Get a reference to the student list on our firebase realtime database
   const reference = ref(db, '/students');
@@ -75,9 +79,10 @@ function writeStudentData(fname, lname, sID, age, gender, sEmail) {
 
 
 // Get reference to the submit button which id = "send"
-let submitButton1 = document.getElementById("send1");
+let submitButton1 = document.getElementById("sendTeacher");
 
 // Add an event listener, which will work when the submit button is clicked
+if (submitButton1 != null) {
 submitButton1.addEventListener("click", (e) => {
   
   //Prevent Default Form Submission Behavior
@@ -97,18 +102,12 @@ submitButton1.addEventListener("click", (e) => {
   // Call to the function writeStudentData, passing two parameters: first and last name
   writeTeacherData(fnameT, lnameT, sIDT, subjectT, genderT, sEmailT);
 
-})
-
-
-//********************* STEP TWO: Create the function that will write the data on the firebase realtime database ***
-
+})}
 
 // Write the student data that comes from the html form
 // We are using two parameters: first name and last name
 function writeTeacherData(fnameT, lnameT, sIDT, subjectT, genderT, sEmailT) {
 
-  // Get the database from our firebase app
-  const db = getDatabase(app);
 
   // Get a reference to the student list on our firebase realtime database
   const reference1 = ref(db, '/teachers');
@@ -122,6 +121,61 @@ function writeTeacherData(fnameT, lnameT, sIDT, subjectT, genderT, sEmailT) {
       gender: genderT,
       schoolEmail: sEmailT,
   });}
+
+//************************************ */ Adding Class
+
+
+// Get reference to the submit button which id = "send"
+let submitButton2 = document.getElementById("sendClass");
+
+// Add an event listener, which will work when the submit button is clicked
+if (submitButton2 != null) {
+submitButton2.addEventListener("click", (e) => {
+  
+  //Prevent Default Form Submission Behavior
+  e.preventDefault();
+  console.log("Test");
+  // Get the value of the name field in the form
+  let cname = document.getElementById("cname").value;
+  let teacher = document.getElementById("teacher").value;
+  let modelURL = document.getElementById("modelURL").value;
+  
+  // Print the name in the console, for testing purposes
+  console.log(cname);
+
+  // Call to the function writeStudentData, passing two parameters: first and last name
+  writeClassData(cname, teacher, modelURL);
+
+})}
+
+// Write the student data that comes from the html form
+// We are using two parameters: first name and last name
+function writeClassData(cname, teacher, modelURL) {
+
+
+  // Get a reference to the student list on our firebase realtime database
+  const reference2 = ref(db, '/classes');
+  onValue(reference2, (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const childKey = childSnapshot.key;
+      const childData = childSnapshot.val();
+      console.log(childKey);
+  console.log(childData);
+    });
+  }, {
+    onlyOnce: true
+  });
+
+
+  // Push the data from the html form to the reference to the student list (see step below)
+  push(reference2, {
+      ClassName: cname,
+      TeacherName: teacher,
+      machineLearningModelURL: modelURL,
+  });}
+
+
+
 
 
 
